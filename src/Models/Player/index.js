560e88b5b7eb3d8c2b4641463bec8059752ab7ee
Igor Simoes <ibps@cin.ufpe.br>
2020-07-31 16:40:58 -0300

    Create player model

---
+++ src/Models/Player/index.js
@@ -0,0 +1,11 @@
+class PLayer {
+  constructor() {
+    this.health = 10;
+    this.energy = 0;
+    this.fame = 0;
+    this.monsterName = '';
+    this.area = '';
+    this.constantPower = [];
+    this.discardPower = [];
+  }
+}
