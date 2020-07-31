77f10320849a49aa542bc6643fdc6fd5d29aff1e
Igor Simoes <ibps@cin.ufpe.br>
2020-07-31 16:41:15 -0300

    Create game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+import Board from './Components/Board';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>BASEMENT</h1>
+        <Board />
       </header>
     </div>
   );
